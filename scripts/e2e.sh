b1573527a6350166338cde4430a3e58e3b4b9032
peefy <xpf6677@163.com>
2024-08-14 10:43:03 +0800

    chore: move kcl-releated examples to the examples_kcl folder
    Signed-off-by: peefy <xpf6677@163.com>

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -7,3 +7,9 @@ find examples -type f -name "Makefile" -exec sh -c '
     cd "$(dirname "$dir")" || exit
     make
 ' \;
+
+find examples_kcl -type f -name "Makefile" -exec sh -c '
+    dir="{}"
+    cd "$(dirname "$dir")" || exit
+    make
+' \;
